01a50683d3d54aeb3556500553e7c791a018e314
dukyeon ha <iribbonkr@gmail.com>
2020-08-19 22:38:36 +0900

    MOVIE SCREEN

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,7 +1,7 @@
 import React, {useLayoutEffect} from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import Movies from "../screens/Movies";
+import Movies from "../screens/Movies/MoviesContainer";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
@@ -41,7 +41,7 @@ export default ({ navigation, route }) => {
               iconName += "tv";
             } else if (route.name === "Search") {
               iconName += "search";
-            } else if (route.name === "Favourites") {
+            } else if (route.name === "Discovery") {
               iconName += "heart";
             }
             return (
